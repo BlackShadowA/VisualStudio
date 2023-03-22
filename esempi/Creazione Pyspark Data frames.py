b0ce31541df9a40c3678b4d627ee1dff9196e777
ur00601 <gaetano.mauro@unicredit.eu>
2023-03-22 11:54:33 +0100

    nuovo file

---
+++ esempi/Creazione Pyspark Data frames.py
@@ -0,0 +1,22 @@
+import findspark
+findspark.init()
+import pyspark
+from pyspark.sql.session import SparkSession
+import pandas as pd
+from pyspark.sql import functions as F
+
+spark = SparkSession\
+    .builder\
+    .appName("PySpark XGBOOST Native")\
+    .getOrCreate()
+
+
+print(f"Versione Pyspark = {spark.version}")
+
+my_dict = {
+    'key':[10,20,30],
+    'valori':[50,None,10]
+}
+
+ll = spark.createDataFrame(pd.DataFrame(my_dict))
+ll.show()
