3aae1da0c5459590766288e3c2c997a3b7017a54
ur00601 <gaetano.mauro@unicredit.eu>
2023-06-19 08:30:03 +0200

    nuovo

--- esempi/Pyspark/A02_Pyspark_sas_format.py
+++ esempi/Pyspark/A02_Pyspark_sas_format.py
@@ -27,16 +27,33 @@ print(f"Versione Pyspark = {spark.version}")
 
 my_dict = {
     'ProductType':['CreditExpress','CreditExpress','CreditExpress - Compact','CreditExpress - Compact'],
-    'amount':[4000,30000,40000,100000],
+    'amount':[40000,30000,40000,100000],
     'tasso': [7.25,7.25,8.0,6.0]
 }
 
 ll = spark.createDataFrame(pd.DataFrame(my_dict))
 ll.show()
 
+# Primo metodo
+from itertools import chain
 
+vol_o_w_one = { 40001: 'MOL_DOUBLE_COUNTING',
+                30000: 'MOL_IMPIEGHI_CLIENTELA',
+                100000: 'MOL_RACCOLTA'
+            }
 
+vol_o_w_one = F.create_map([F.lit(x) for x in chain(*vol_o_w_one.items())])
 
+df = ll.withColumn("MOL_ONE", vol_o_w_one.getItem(F.col("amount")))
+
+df.show()
+
+# Secondo metodo con Replacle
+
+df = ll.replace(to_replace=vol_o_w_one, subset='amount')
+df.show()
+
+# con udf
 def condizione(ProductType, amount, tasso):
     if ProductType == 'CreditExpress':
         if amount>=3000 and amount<=5000 and tasso<=7.25:
