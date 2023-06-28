febbf8d8122a09fa5377a5df74b711a832ddae41
ur00601 <gaetano.mauro@unicredit.eu>
2023-06-28 09:18:57 +0200

    ss

--- esempi/Documentazione/R02_Rinomina_molte_colonne.py
+++ esempi/Documentazione/R02_Rinomina_molte_colonne.py
@@ -2,3 +2,13 @@
 rename_col = [f"{e.upper()}_updated" for e in b.columns]
 
 df = df.toDF(*rename_col)
+
+
+# voglio rinominare tutte le colonne che finiscono con _sum 
+
+def col_replace(df):
+    replacements = {c:c.replace('_sum','') for c in df.columns if '_sum' in c}
+    df = df.select([col(c).alias(replacements.get(c, c)) for c in df.columns])
+    return df
+df = col_replace(df)
+df.show()
