99a23fcd7fa593c33231dd209bdaf7ee30d6f6ec
ur00601 <gaetano.mauro@unicredit.eu>
2023-08-09 12:43:46 +0200

    con regressione

--- esempi/Feature_Selection/Mrmr_Feature_selection/A001A1_MRMR_Feature_selection_pyspark.py
+++ esempi/Feature_Selection/Mrmr_Feature_selection/A001A1_MRMR_Feature_selection_pyspark.py
@@ -5,7 +5,7 @@ from pyspark.sql.session import SparkSession
 import pandas as pd
 from pyspark.sql import functions as F
 import os
-from Mrmr_pyspask import mrmr_classif
+from Mrmr_pyspask import mrmr_classif, mrmr_regression
 os.environ["HADOOP_HOME"]= 'C:\\Travaux_2012\\Anaconda e Python\\hadoop-2.8.1'
 
 
@@ -23,6 +23,17 @@ filename = 'C:\\Users\\ur00601\\Downloads\\Monotonic-Optimal-Binning-main\\data\
 df = spark.read.csv(filename, header=True,inferSchema=True, sep=';')\
     .withColumn('default', F.col('default') + F.lit(-1))
     
-    
-selected_features = mrmr_classif(df = df, target_column="default", K=2)
+# classificazione    
+selected_features = mrmr_classif(df = df, target_column="default", K=3)
 print(selected_features)
+
+#  Regressione
+file = "C:\\Travaux_2012\\Esempi_python\\train.csv"
+df2 = spark.read.csv(file, header=True,inferSchema=True, sep=',')
+df2.show()
+
+selected_features_regression = mrmr_regression(df = df2, target_column="SalePrice", K=3)
+print(selected_features_regression)
+
+
+
