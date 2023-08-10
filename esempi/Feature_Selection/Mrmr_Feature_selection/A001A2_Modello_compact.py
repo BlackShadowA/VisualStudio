319305794ea7155e7eca4b91d50e55e80d6a1f76
ur00601 <gaetano.mauro@unicredit.eu>
2023-08-10 11:15:07 +0200

    lllllllllllllllllllllllllllllllllll

---
+++ esempi/Feature_Selection/Mrmr_Feature_selection/A001A2_Modello_compact.py
@@ -0,0 +1,38 @@
+import findspark
+findspark.init()
+import pyspark
+from pyspark.sql.session import SparkSession
+import pandas as pd
+from pyspark.sql import functions as F
+import os
+from Mrmr_pyspask import mrmr_classif, mrmr_regression
+from woe_pyspark import var_type,WOE
+from plot_woe import plotBinsSummary
+os.environ["HADOOP_HOME"]= 'C:\\Travaux_2012\\Anaconda e Python\\hadoop-2.8.1'
+
+
+
+#Spark Ui http://localhost:4040
+spark = SparkSession\
+    .builder\
+    .appName("PySpark XGBOOST Native")\
+    .getOrCreate()
+    
+    
+# modello compact
+filename_compact = 'C:\\Users\\ur00601\\Downloads\\Feature importance mrmr.csv'
+df3 = spark.read.csv(filename_compact , header=True,inferSchema=True, sep=',')\
+           .drop('n_pp')
+
+from woe_pyspark import var_type,WOE
+selected_features = mrmr_classif(df = df3, target_column="label", K=3)
+print(selected_features)
+
+# Woe e IV delle variabili selezionate
+target = 'label'
+max_bin = 5
+ll ,pp = WOE(df3, selected_features, target, max_bin)
+ll = ll.sort('varname', 'start')
+ll.show()
+pp.show()
+plotBinsSummary(ll.toPandas(), var_name = 'importo_erogato_last_pp')
