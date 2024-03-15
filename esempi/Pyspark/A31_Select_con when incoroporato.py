ec69365d3ecfb14a9a7b1541e575d4b2185c3605
ur00601 <gaetano.mauro@unicredit.eu>
2024-03-15 08:20:41 +0100

    last_last_last

--- esempi/Pyspark/A31_Select_con when incoroporato.py
+++ esempi/Pyspark/A31_Select_con when incoroporato.py
@@ -37,3 +37,19 @@
         )
         .distinct()
     )
+
+
+# Dicotomica
+        .select('cust_id',
+                F.expr("tp_user_olb_trim in ('D', 'I')").cast('integer').alias('90_days_active_digital_users'),
+                F.expr("tp_user_dsk_trim in ('D', 'I')").cast('integer').alias('90_days_active_online_users'),
+                F.concat("tp_user_aps_trim", "tp_user_apt_trim", "tp_user_msi_trim", "tp_user_bbk_trim").rlike(r'(D|I)').cast('integer').alias('90_days_active_mobile_users'),
+                F.concat("tp_user_aps_trim", "tp_user_bbk_trim").rlike(r'(D|I)').cast('integer').alias('90_days_active_app_users'),
+                F.expr("tp_user_olb in ('D', 'I')").cast('integer').alias('30_days_active_digital_users'),
+                F.expr("tp_user_dsk in ('D', 'I')").cast('integer').alias('30_days_active_online_users'),
+                F.concat("tp_user_aps", "tp_user_apt", "tp_user_msi", "tp_user_bbk").rlike(r'(D|I)').cast('integer').alias('30_days_active_mobile_users'),
+                F.concat("tp_user_aps", "tp_user_bbk").rlike(r'(D|I)').cast('integer').alias('30_days_active_app_users'),
+            )
+        
+        
+# Nota non la puoi utilizzare con selectExpr
